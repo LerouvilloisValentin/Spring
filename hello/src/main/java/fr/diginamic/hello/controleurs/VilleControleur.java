0a517ecfc50b2328c5d7f2d1db3016ffdd79b778
LerouvilloisValentin <lerouvillois.valentin61@hotmail.fr>
2024-07-03 12:18:24 +0200

    tp3 controller spring done

--- hello/src/main/java/fr/diginamic/hello/controleurs/VilleControleur.java
+++ hello/src/main/java/fr/diginamic/hello/controleurs/VilleControleur.java
@@ -1,5 +1,24 @@
 package fr.diginamic.hello.controleurs;
 
-public class VilleControleur {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.diginamic.hello.dao.Ville;
+
+@RestController
+@RequestMapping("/villes")
+public class VilleControleur {
+	@GetMapping
+	public List<Ville> getVille(){
+        List<Ville> villes = new ArrayList<>();
+        villes.add(new Ville("Paris", 2148000));
+        villes.add(new Ville("Marseille", 861635));
+        villes.add(new Ville("Lyon", 513275));
+
+        return villes;
+	}
 }
